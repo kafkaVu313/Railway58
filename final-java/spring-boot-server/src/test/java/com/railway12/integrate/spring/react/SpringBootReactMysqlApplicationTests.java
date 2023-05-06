00a0f77368f93cb79906735811f57d7ca3e08d5b
Huyen Nguyen <huyennguyen@192.168.2.2>
2023-05-06 20:36:10 +0700

    source code java final

---
+++ final-java/spring-boot-server/src/test/java/com/railway12/integrate/spring/react/SpringBootReactMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.railway12.integrate.spring.react;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootReactMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
